7c80dd007654535f79451b7f968de5413d2aa41a
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2024-02-16 20:15:02 +0100

    Adjust diesel card

--- components/pages/seller/Product.tsx
+++ components/pages/seller/Product.tsx
@@ -7,26 +7,28 @@ import Image from "next/image";
 
 const Product = () => {
   return (
-    <div className="flex w-full items-center justify-between gap-6 rounded-md bg-light-900 px-2 py-4 md:p-4">
+    <div className="flex w-full items-center justify-between gap-3 rounded-md bg-light-900 p-3 max-xs:text-[0.8rem] md:p-4">
       <div>
         <span className="font-medium lg:text-[1.125rem]">
           Diesel (Density 0.85g/ml)
         </span>
       </div>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-2 md:gap-4">
         <span className="font-semibold lg:text-[1.125rem]">
           {formatPrice(1050)}
         </span>
-        <Button className="border bg-primary-500 text-light-900 md:h-[2.375rem]">
+        <Button className="h-[1.8rem] w-[5rem] border bg-primary-500 text-light-900 md:h-[2.375rem]">
           Update
         </Button>
-        <span className="size-5 rounded-full bg-primary-500"></span>
-        <Image
-          src="/images/icons/menu-dot.svg"
-          width={20}
-          height={20}
-          alt="menu"
-        />
+        <div className="size-5 shrink-0 rounded-full bg-primary-500 max-sm:size-2"></div>
+        <div className="shrink-0 max-xs:mr-2">
+          <Image
+            src="/images/icons/menu-dot.svg"
+            width={20}
+            height={20}
+            alt="menu"
+          />
+        </div>
       </div>
     </div>
   );
