ec6d3eda800fed22b5700a56f46adca87c1725d8
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2024-03-13 19:29:05 +0100

    fix unused definition error

--- components/shared/table/AdminSellerOrders.tsx
+++ components/shared/table/AdminSellerOrders.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import {
