df55406c77b727bad6c20c1898fdeb5c2d1181c9
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2024-03-16 00:20:59 +0100

    Implement download transaction receipt

--- components/shared/menubar/TransactionActionMenu.tsx
+++ components/shared/menubar/TransactionActionMenu.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 import { Transaction } from "@prisma/client";
 import {
   Menubar,
@@ -9,14 +12,27 @@ import {
   MenubarMenu,
   MenubarTrigger,
 } from "@/components/ui/menubar";
-// import DialogWrapper from "../dialog/DialogWrapper";
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { formatDate, formatPrice, getTimeOfDay } from "@/lib/utils";
 
 const TransactionActionMenu = ({
   transaction,
 }: {
   transaction: Transaction;
 }) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleDownloadPDF = () => {
+    const input = document.getElementById("pdf-content");
+    // Specify the id of the element you want to convert to PDF
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "PNG", 0, 0);
+      pdf.save(`${transaction.reference}-reciept.pdf`);
+    });
+  };
+
   return (
     <div className="">
       <Menubar className="border-none">
@@ -32,7 +48,7 @@ const TransactionActionMenu = ({
           </MenubarTrigger>
           <MenubarContent className="bg-light-900">
             <MenubarItem
-              onClick={() => {}}
+              onClick={() => setIsDialogOpen(true)}
               className="flex cursor-pointer items-center gap-5"
             >
               <Image
@@ -47,15 +63,97 @@ const TransactionActionMenu = ({
           </MenubarContent>
         </MenubarMenu>
       </Menubar>
-      {/* <DialogWrapper
-        title=""
-        dialogState={isDialogOpen}
-        customClose={false}
-        containerStyle="max-sm:w-[18.75rem] sm:max-w-[24.375rem]"
-        handleDialogState={() => {}}
-      >
-        
-      </DialogWrapper> */}
+
+      {isDialogOpen && (
+        <div className="fixed inset-0 z-[1300] flex h-screen items-center justify-center bg-[rgba(0,0,0,0.6)]">
+          <div
+            className={`z-[1500] mx-auto rounded-[8px] bg-white p-0 pb-8 max-sm:w-[18.75rem] sm:w-[24.375rem]`}
+          >
+            <div id="pdf-content" className="mx-auto w-full pb-6">
+              <div className="m-0 rounded-t-[8px] bg-primary-100 p-2 text-black md:p-3">
+                <div className="flex w-full justify-between text-[0.88rem] font-medium">
+                  <span>Diesel NG</span>
+                  <Button
+                    onClick={() => setIsDialogOpen(false)}
+                    type="button"
+                    className="size-max p-0"
+                  >
+                    <Image
+                      src="/images/icons/cancel.svg"
+                      width={14}
+                      height={14}
+                      alt="cancel"
+                    />
+                  </Button>
+                </div>
+                <div>
+                  <span className="text-[0.6rem]">Payment Details</span>
+                </div>
+                <div className="mt-6 flex flex-col items-center">
+                  <span className="text-[0.75rem] font-medium">Amount:</span>
+                  <span className="font-fraunces text-[1.25rem] text-primary-500 md:text-[1.5rem]">
+                    {formatPrice(Number(transaction.amount))}
+                  </span>
+                </div>
+              </div>
+              <div className="mt-6 space-y-5 px-3 text-[0.75rem] md:px-6">
+                <div className="flex items-center justify-between">
+                  <span className="font-medium">Date</span>
+                  <span className="font-semibold text-black">
+                    {formatDate(transaction.date)}
+                  </span>
+                </div>
+                <div className="flex items-center justify-between">
+                  <span className="font-medium">Time</span>
+                  <span className="font-semibold text-black">
+                    {getTimeOfDay(Number(transaction.date))}
+                  </span>
+                </div>
+                <div className="flex items-center justify-between">
+                  <span className="font-medium">Reference</span>
+                  <span className="font-semibold text-black">
+                    {transaction.reference}
+                  </span>
+                </div>
+                <div className="flex items-center justify-between">
+                  <span className="font-medium">Order Number</span>
+                  <span className="font-semibold text-black">
+                    {transaction.orderNumber}
+                  </span>
+                </div>
+                <div className="flex items-center justify-between">
+                  <span className="font-medium">Name</span>
+                  <span className="font-semibold text-black">
+                    {transaction?.buyer
+                      ? transaction?.buyer?.businessName
+                      : transaction?.seller?.businessName}
+                  </span>
+                </div>
+                <div className="flex items-center justify-between">
+                  <span className="font-medium">Channel</span>
+                  <span className="font-semibold text-black">
+                    {transaction.channel}
+                  </span>
+                </div>
+                <div className="flex items-center justify-between">
+                  <span className="font-medium">Category</span>
+                  <span className="font-semibold text-black">
+                    {transaction.category}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center">
+              <Button
+                onClick={handleDownloadPDF}
+                className="bg-primary-500 text-light-900 active:bg-primary-400"
+              >
+                Download
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
