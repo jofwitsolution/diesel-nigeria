c4ea28c0823ef97d8e683e5e940b270c11dae43c
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2024-04-04 15:13:47 +0100

    Remove user retension value

--- components/pages/admin/RetentionBoard.tsx
+++ components/pages/admin/RetentionBoard.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
-import { formatPrice } from "@/lib/utils";
 
 const RetentionBoard = () => {
   return (
@@ -10,27 +9,19 @@ const RetentionBoard = () => {
         <div className="flex w-full flex-wrap">
           <div className="flex h-[9.375rem] flex-1 flex-col gap-5 rounded-l-lg border bg-primary-500 p-4 text-light-900">
             <span className="font-medium">Users</span>
-            <span className="font-fraunces md:text-[1.5rem]">
-              {formatPrice(500000)}
-            </span>
+            <span className="font-fraunces md:text-[1.5rem]">0</span>
           </div>
           <div className="flex h-[9.375rem] flex-1 flex-col gap-5 border p-4">
             <span className="font-medium">Buyers</span>
-            <span className="font-fraunces md:text-[1.5rem]">
-              {formatPrice(500000)}
-            </span>
+            <span className="font-fraunces md:text-[1.5rem]">0</span>
           </div>
           <div className="flex h-[9.375rem] flex-1 flex-col gap-5 border  p-4">
             <span className="font-medium">Sellers</span>
-            <span className="font-fraunces md:text-[1.5rem]">
-              {formatPrice(500000)}
-            </span>
+            <span className="font-fraunces md:text-[1.5rem]">0</span>
           </div>
           <div className="flex h-[9.375rem] flex-1 flex-col gap-5 rounded-r-lg border p-4">
             <span className="font-medium">User Retension</span>
-            <span className="font-fraunces md:text-[1.5rem]">
-              {formatPrice(500000)}
-            </span>
+            <span className="font-fraunces md:text-[1.5rem]">0</span>
           </div>
         </div>
       </CardContent>
